081d6ee9dfd2337ba9e0ccc78463463b7b96bf27
Xurboise <etchboom@gmail.com>
2023-10-04 07:00:42 -0400

    new files and modded main

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,19 +2,13 @@ import os
 import csv
 months = []
 profLoss = []
-average = 69
-increaseDate = "Date1"
-increaseNum = 999
-decreaseDate = "Date2"
-decreaseNum = -999
-
-csvpath = os.path.join('PyBank', 'Resources', 'budget_data.csv')
+change = []
+csvpath = os.path.join('GitHub','python-challenge','PyBank', 'Resources', 'budget_data.csv')
+#C:\Users\etchb\Documents\GitHub\python-challenge\PyBank\Resources\budget_data.csv
 
 with open(csvpath) as csvfile:
-    # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvfile)
-    print(f"Header: {csv_header}")
 
     for row in csvreader:
         months.append(row[0])
@@ -22,19 +16,35 @@ with open(csvpath) as csvfile:
 
         total = sum(profLoss)
 
+for i in range(0, len(profLoss), 1):
+
+    if(i < len(profLoss) - 1):
+        change.append((profLoss[i+1]) - profLoss[i])
+
+increaseNum = max(change)
+decreaseNum = min(change)
+indexHigh = change.index(max(change))
+indexLow = change.index(min(change))
+increaseDate = months[indexHigh + 1]
+decreaseDate = months[indexLow + 1]          
+average = sum(change) / len(change)
 
 print("Finacial Analysis")
 print("----------------------------")
 print(f"Total Months: {len(months)}")
-
 print(f"Total: ${total}")
+print(f"Average Change: ${round(average, 2)}")
+print(f"Greatest Increase in Profit: {increaseDate} (${increaseNum})")
+print(f"Greatest Decrease in Profit: {decreaseDate} (${decreaseNum})")
 
-print("Average Change: $" + str(average))
-
-
-
-print(f"Greatest Increase in Profit:{increaseDate} ({increaseNum})")
-
-print(f"Greatest Decrease in Profit: {decreaseDate} ({decreaseNum})")
-
+output = os.path.join('PyBank', 'analysis', 'analysis.txt')
 #output_file = os.path.join("output.csv") but to text
+with open(output, 'w') as txtfile:
+    txtwriter = csv.writer(txtfile, delimiter=',')
+    txtwriter.writerow("Finacial Analysis")
+    txtwriter.writerow("----------------------------")
+    txtwriter.writerow(f"Total Months: {len(months)}")
+    txtwriter.writerow(f"Total: ${total}")
+    txtwriter.writerow(f"Average Change: ${round(average, 2)}")
+    txtwriter.writerow(f"Greatest Increase in Profit: {increaseDate} (${increaseNum})")
+    txtwriter.writerow(f"Greatest Decrease in Profit: {decreaseDate} (${decreaseNum})")
